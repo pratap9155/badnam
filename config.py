75ad0f5229cc7d1063cf9772aff99e2073f7db2b
Venom Pratap <venompratap22@gmail.com>
2025-08-07 14:41:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/About_badnam_xd") 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+G-kGeNJKv_YxNTg9")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/venompratap") 
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/venompratapchat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/bs5gni.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/6jjfrx.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/fa1xas.jpg"
@@ -120,3 +120,4 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
